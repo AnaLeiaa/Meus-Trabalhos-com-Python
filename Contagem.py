6c27fe169130df37e632fde509b9b0c55f99bab6
AnaLeiaa <analeiahhh@gmail.com>
2026-04-08 20:31:03 -0300

    Trabalhos com Python

---
+++ Contagem.py
@@ -0,0 +1,10 @@
+print('---Contagem------')
+i = int(input('Digite o número Inicial: '))
+f = int(input('Digite o número Final :'))
+p = int(input('De quantas e quantas casas?:'))
+for c in range(i, f + 1, p):
+    print(c)
+print('FIM')
+
+
+
